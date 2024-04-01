b2c43b4b27f69a1027d8b4d20b8c2d4b7a7b1938
Simmo <2524333609@qq.com>
2024-04-01 15:36:58 +0800

    add development command prompt script(win32)

---
+++ mmenv.bat
@@ -0,0 +1,10 @@
+@echo off
+set METAMOD_ROOT=%~dp0
+set MMSOURCE112=%METAMOD_ROOT%metamod-sdk
+set HL2SDKCS2=%METAMOD_ROOT%hl2sdk-cs2
+
+echo **********************************************************************
+echo ** Metamod Development Command Prompt by Simmo
+echo ** Metamod SDK Version: v2.0.0-dev
+echo ** HL2 SDK Version: CS2
+echo **********************************************************************
